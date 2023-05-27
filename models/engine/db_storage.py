1a0d166bad234359e18fbb3d1b89ba4e93813586
emmylem <benjaminosondu55@gmail.com>
2023-05-27 15:56:26 +0000

    update storage_get_count

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,14 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Fetches a single object."""
+        if id and cls:
+            one = "{}.{}".format(id, cls)
+            obj = self
+        return None
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage."""
+        return len(self.all(cls))
