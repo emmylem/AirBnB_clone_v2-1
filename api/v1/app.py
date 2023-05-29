1f1104b4b6061f134ca463c4b6eba08b5ba9e8f1
emmylem <benjaminosondu55@gmail.com>
2023-05-29 12:04:20 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,8 @@ from flask import Flask, jsonify, make_response
 from flask_cors import CORS, cross_origin
 from flask import Blueprint
 from models import storage
+from werkzeug.exceptions import HTTPException
+from flasgger import Swagger
 import os
 
 app = Flask(__name__)
@@ -15,13 +17,15 @@ host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', '5000')
 
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+swagger = Swagger(app)
+app.url_map.strict_slashes = False
 
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_app(self):
+def teardown_app(exception):
     """
     tearsdown app context.
     """
@@ -33,7 +37,7 @@ def notfound_404(exception):
     """
     handles 404 errors.
     """
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
